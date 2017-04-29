6270a354cd7166ac12db919e9df4175f2a46ba0e
Genevieve Conty <genevieve.conty@AV-MBP-7208.attlocal.net>
2017-04-29 18:21:51 -0400

    changed to port 80

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/', function (req, res) {
 
 });
 
-app.listen(3000);
-console.log("on port 3000");
+app.listen(80);
+console.log("80");
 
 // var app = express();
 // // Sets an initial port. We'll use this later in our listener
